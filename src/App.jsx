f5343ba9bfee75d14e8fb69bec4ce5335947a505
chrvnixx <emmanueladekola91@gmail.com>
2026-03-24 10:18:08 +0100

    Create analytics page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
 import OverviewPage from "./Pages/OverviewPage";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/common/Sidebar";
+import ProductsPage from "./Pages/ProductsPage";
+import UsersPage from "./Pages/UsersPage";
+import SalesPage from "./Pages/SalesPage";
+import AnalyticsPage from "./Pages/AnalyticsPage";
+import SettingsPage from "./Pages/SettingsPage";
+import OrdersPage from "./Pages/OrdersPage";
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
 
       <Routes>
         <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </div>
   );
--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -8,8 +8,8 @@ import {
   TrendingUp,
   Users,
 } from "lucide-react";
-import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router";
 
 const SIDEBAR_ITEMS = [
@@ -27,11 +27,14 @@ const SIDEBAR_ITEMS = [
   { name: "Settings", icon: Settings, color: "#6EE7B7", href: "/settings" },
 ];
 
-export default function Sidebar() {
+const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
     <motion.div
-      className={`relative z-10 transition-all duration-300 ease-in-out shrink-0  ${isSidebarOpen ? "w-64" : "w-20"}`}
+      className={`relative z-10 transition-all duration-300 ease-in-out shrink-0 ${
+        isSidebarOpen ? "w-64" : "w-20"
+      }`}
       animate={{ width: isSidebarOpen ? 256 : 80 }}
     >
       <div className="h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700">
@@ -43,6 +46,7 @@ export default function Sidebar() {
         >
           <Menu size={24} />
         </motion.button>
+
         <nav className="mt-8 grow">
           {SIDEBAR_ITEMS.map((item) => (
             <Link key={item.href} to={item.href}>
@@ -71,4 +75,5 @@ export default function Sidebar() {
       </div>
     </motion.div>
   );
-}
+};
+export default Sidebar;
