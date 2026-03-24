b3a379ae4880dbdbfd5e8f974f960cd5f43dd832
chrvnixx <emmanueladekola91@gmail.com>
2026-03-24 09:40:57 +0100

    update sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="flex h-sceen bg-gray-900 text-gray-100 overflow-hidden">
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
       {/* background */}
-      <div className="fixed inset-0 z-0">
-        <div className="absolute inset-0 bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"></div>
-        <div className="absolute inset-0 backdrop-blur-sm"></div>
-      </div>
 
       <Sidebar />
 
--- src/Pages/OverviewPage.jsx
+++ src/Pages/OverviewPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function OverviewPage() {
-  return <div></div>;
+  return <div>OverviewPage</div>;
 }
